00c1121cd05110af47f7afc67594610b0896c17b
rystnk <82385052+rystnk@users.noreply.github.com>
2023-07-07 21:55:33 +0300

    Update select_1.sql

--- select_1.sql
+++ select_1.sql
@@ -18,6 +18,11 @@ FROM songwriter s
 WHERE length(s.name) - length(replace(s.name, ' ', '')) = 0;
 
 
-SELECT t."name"
-FROM track t
-WHERE lower(t."name") like '%мой%' or lower(t."name") like '%my%';
+WITH cte_1 AS
+(SELECT 
+substring(t."name" FROM '[а-яА-Яa-zA-Z\s]+') AS nname, id_track
+FROM track t)
+SELECT tr."name"
+FROM cte_1 ct
+JOIN track tr ON ct.id_track = tr.id_track
+WHERE string_to_array(lower(ct.nname), ' ') && ARRAY['мой', 'my'];
